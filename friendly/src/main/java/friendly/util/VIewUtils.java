6841b7d23e6f11991856cd53fe293285d9b5fe98
gwh <gwh@riseup.net>
2015-03-16 23:53:40 +0000

    Added ViewUtils class.

---
+++ friendly/src/main/java/friendly/util/VIewUtils.java
@@ -0,0 +1,45 @@
+package friendly.util;
+
+import android.support.annotation.NonNull;
+import android.view.View;
+
+/**
+ * Utility class for View methods.
+ */
+public final class ViewUtils {
+
+    private ViewUtils() {
+        // restrict instantiation
+    }
+
+    /**
+     * Sets the views to VISIBLE.
+     */
+    public static void makeVisible(@NonNull View... views) {
+        for (View view : views) {
+            view.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Sets the Views to INVISIBLE.
+     */
+    public static void makeInvisible(@NonNull View... views) {
+        for (View view : views) {
+            if (view != null) {
+                view.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    /**
+     * Sets the Views to GONE.
+     */
+    public static void makeGone(@NonNull View... views) {
+        for (View view : views) {
+            if (view != null) {
+                view.setVisibility(View.GONE);
+            }
+        }
+    }
+}
